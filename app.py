81019e689a29582054be1f64778992dc6950107f
gabrielhm1 <gabriel.matos1200@gmail.com>
2022-09-22 22:12:09 -0300

    Criacao de uma segunda rota

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/odds/<int:Number>')
+def odd(Number):
+    print(Number)
+    content = {
+        "id" : Number
+    }
+    return render_template("odd.html", content = content)
 if __name__ == "__main__":
-    app.run(debug=true)
+    app.run(debug=True)
