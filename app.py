e304a12480c9fe724fde1d18eccc9ccdcec42d5e
gabrielhm1 <gabriel.matos1200@gmail.com>
2022-10-10 23:47:36 -0300

    Adicionada função que compara as odds

--- app.py
+++ app.py
@@ -1,28 +1,71 @@
+import re
 from flask import Flask,render_template
 # from datetime import date
 from datetime import datetime,date
+
+from sqlalchemy import null
 from models.tables import db
-from models.tables import Match
+from models.tables import Match,Odd
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db.init_app(app)
+def compare_odds(match_id):
+    best_odd = {
+        "host_win": {
+            "odd": 0
+        },
+        "away_win": {
+            "odd": 0
+        },
+        "draw":{
+            "odd": 0
+        }
+    }
+    odds = Odd.query.filter_by(match_id = match_id)
+    for odd in odds:
+        if odd.host_win > best_odd["host_win"]["odd"]:
+            best_odd["host_win"]["odd"] = odd.host_win
+            best_odd["host_win"]["company"] = odd.company
+        if odd.away_win > best_odd["host_win"]["odd"]:
+            best_odd["away_win"]["odd"] = odd.away_win
+            best_odd["away_win"]["company"] = odd.company
+        if odd.match_draw > best_odd["host_win"]["odd"]:
+            best_odd["match_draw"]["odd"] = odd.match_draw
+            best_odd["match_draw"]["company"] = odd.company
+    return best_odd
+
+def get_best_odd(match_id=null,matchs=null):
+    if matchs == null:
+        best_odd = compare_odds(match_id)
+        return best_odd
+    elif match_id == null:
+        matchs_info = []
+        for match in matchs:
+            best_odd = get_best_odd(match_id=match.id)
+            matchs_info.append(vars(match).update(best_odd))
+        return matchs_info
 
 
 @app.route('/')
 def index():
     date_today = date.today()
-
     matchs = Match.query.filter( date_today <= Match.match_date).all()
+    matchs_info  = get_best_odd(matchs=matchs)
 
-    return render_template("index.html",matchs=matchs)
+    return render_template("index.html",content=matchs_info)
 
-@app.route('/odds/<int:Number>')
-def odd(Number):
-    print(Number)
+@app.route('/odds/<int:match_id>')
+def odd(match_id):
+    odds = Odd.query.filter_by(match_id = match_id)
+    match = Match.query.filter_by(id=match_id)
+
     content = {
-        "id" : Number
+        "best_odd": get_best_odd(match_id=match_id),
+        "odds": odds,
+        "match":match  
     }
+
     return render_template("odd.html", content = content)
 
 if __name__ == "__main__":
