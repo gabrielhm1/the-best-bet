48a14ff14d04d1d984c30bb87d0dfab5446fb928
thiagoricardop <t.ricardooliveira08@gmail.com>
2022-10-25 19:48:25 -0300

    Padronizando os dados de saída

--- bot/bet365_bot.py
+++ bot/bet365_bot.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 
+#cria uma lista das partidas
 partidas = []
 
 with open(r"C:\Users\trica\Downloads\bet.html", encoding="utf8") as fp:
@@ -15,20 +16,26 @@ with open(r"C:\Users\trica\Downloads\bet.html", encoding="utf8") as fp:
             #"odd_visitante":123
         }
         times = item.find_all("div", class_="rcl-ParticipantFixtureDetailsTeam")
-        dados["mandante"] = times[0].text
-        dados["visitante"] = times[1].text
+        dados["host_team"] = times[0].text
+        dados["away_team"] = times[1].text
         
+        odds = {}
         for index_odd,odd in enumerate(soup.find_all("div", class_="sgl-MarketOddsExpand")):
-            print("odd", index_odd)
             odd_valor = odd.find_all("span", class_="sgl-ParticipantOddsOnly80_Odds")
-        
+            
             if index_odd == 0:
-                dados["odd_mandante"] = odd_valor[index].text
+                odds["host_win"] = odd_valor[index].text
             elif index_odd == 1:
-                dados["odd_empate"] = odd_valor[index].text
+                odds["match_draw"] = odd_valor[index].text
             elif index_odd == 2:
-                dados["odd_visitante"] = odd_valor[index].text
-        partidas.append(dados)
+                odds["away_win"] = odd_valor[index].text
+            
+        
+            odds['company'] = 'bet365'
+
+            odd_dict = {'odds':{**odds}}
+            match_info = dict(dados, **odd_dict)
+        partidas.append(match_info)
 
 print(partidas)
         
