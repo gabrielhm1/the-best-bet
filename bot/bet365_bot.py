e4a02cdf774acd277c695cbe8d787c4b0ae62b82
thiagoricardop <t.ricardooliveira08@gmail.com>
2022-10-25 20:06:38 -0300

    Comentando o código para entendimento de devs

--- bot/bet365_bot.py
+++ bot/bet365_bot.py
@@ -1,25 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
 
-#cria uma lista das partidas
+#criar uma lista das partidas
 partidas = []
 
+#obter html do site
 with open(r"C:\Users\trica\Downloads\bet.html", encoding="utf8") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
     
+    #localizar os dados necessários através de classes identificadoras e formatar para obter a saída desejada
     for index,item in enumerate(soup.find_all("div", class_="rcl-ParticipantFixtureDetailsAggregateScore")):
         print("partida", index)
         dados = {
             #"time1":"nome_time1",
             #"time2":"nome_time2",
-            #"odd_mandante":123
-            #"odd_empate":123
-            #"odd_visitante":123
+            #odds{
+            #   "odd_mandante":123
+            #   "odd_empate":123
+            #   "odd_visitante":123
+            #}
         }
+        #encontrar os nomes dos times que vão se enfrentar
         times = item.find_all("div", class_="rcl-ParticipantFixtureDetailsTeam")
         dados["host_team"] = times[0].text
         dados["away_team"] = times[1].text
         
+        #encontrar as odds para vitória de cada time e empate
         odds = {}
         for index_odd,odd in enumerate(soup.find_all("div", class_="sgl-MarketOddsExpand")):
             odd_valor = odd.find_all("span", class_="sgl-ParticipantOddsOnly80_Odds")
@@ -36,10 +42,12 @@ with open(r"C:\Users\trica\Downloads\bet.html", encoding="utf8") as fp:
 
             odd_dict = {'odds':{**odds}}
             match_info = dict(dados, **odd_dict)
+        
+        #adicionar cada partida no final da lista 'partidas'
         partidas.append(match_info)
 
+#enviar os dados para a aplicação
 for partida in partidas:
     print(partida)
     rqs = requests.post('http://localhost:5000/match', json=partida)
     print(rqs.text)
-  
