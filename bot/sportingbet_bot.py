3e4f0e97060151501f0656a56d73f1faf985b5d9
yurijun8 <yurijunior8@gmail.com>
2022-10-24 22:19:59 -0300

    Atualização bot sporting bet

--- bot/sportingbet_bot.py
+++ bot/sportingbet_bot.py
@@ -1,11 +1,13 @@
-from bs4 import BeautifulSoup
+ from bs4 import BeautifulSoup
+import requests
 partidas = []
 
-with open(r"sportingbet.html", encoding="utf8") as fp:
+with open(r"C:\Users\caio2\Downloads\sportingbet.html", encoding="utf8") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
     
     for index,item in enumerate(soup.find_all("div", class_="participants-pair-game")):
         print("partida", index)
+        odds = {}
         dados = {
             #"time1":"nome_time1",
             #"time2":"nome_time2",
@@ -14,19 +16,25 @@ with open(r"sportingbet.html", encoding="utf8") as fp:
             #"odd_visitante":123
         }
         times = item.find_all("div", class_="participant")
-        dados["mandante"] = times[0].text
-        dados["visitante"] = times[1].text
+        dados["host_team"] = times[0].text
+        dados["away_team"] = times[1].text
         
         
         for index_odd,odd in enumerate(soup.find_all("div", class_="option option-value ng-star-inserted")):
             print(index_odd,odd.text)
             if index_odd%3 == 0:
-                dados["odd_mandante"] = odd.text
+                odds["host_win"] = odd.text
             elif index_odd%3 == 1:
-                dados["odd_empate"] = odd.text
+                odds["match_draw"] = odd.text
             elif index_odd%3 == 2:
-                dados["odd_visitante"] = odd.text
+                odds["away_win"] = odd.text
                 break
+            odds['company'] = 'sportingbet'
+            odd_dict = {'odds':odds}
+            match_info = dict(dados, **odd_dict)
         print(dados)
-        partidas.append(dados)
-print(partidas)
+        partidas.append(match_info)
+        
+for partida in partidas:
+    rqs = requests.post('http://localhost:5000/match', json=partida)
+    print(rqs.text)
